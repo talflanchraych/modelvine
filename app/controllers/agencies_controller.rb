2f2f1f217170bcdfd6f1caf4f565999977a38ca4
Jinru Wu <woodbleak@gmail.com>
2013-12-07 13:18:47 -0500

    Agencies controller

---
+++ app/controllers/agencies_controller.rb
@@ -0,0 +1,25 @@
+class AgenciesController < ApplicationController
+	def edit
+		@agency = Agency.find(params[:id])
+	end
+
+	def update
+		agency = Agency.find(params[:id])
+		if agency.update_attributes(agency_params)
+			if current_user.approved == false
+				redirect_to waitlist_path 
+			else
+				redirect_to current_user
+			end
+		else
+			render 'edit'
+		end 
+	end
+
+
+	private
+
+	def agency_params
+      params.require(:agency).permit(:bio, :website)
+    end
+end
