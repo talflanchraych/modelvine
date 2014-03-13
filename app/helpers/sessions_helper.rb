53f446258bbea60da2f3e3583c2d93ecf8badf59
Thomas Surgent <surgentt@gmail.com>
2014-03-12 20:46:49 -0400

    working nav

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,7 +15,6 @@ module SessionsHelper
     session[:return_to] = request.url if request.get?
   end
 
-
   ##Lots of old Code here from Pre Devise
 	
  # def sign_in(user)
@@ -39,8 +38,6 @@ module SessionsHelper
   #   remember_token = User.encrypt(cookies[:remember_token])
   #   @current_user ||= User.find_by(remember_token: remember_token)
   # end
-
-
 
   # before Filter to check if User is signed in
   # def signed_in_user
