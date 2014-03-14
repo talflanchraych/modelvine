1c68b963dcb35dbef7a43a7bc98343b00d2147d9
Thomas Surgent <surgentt@gmail.com>
2014-03-14 11:12:22 -0400

    redirect signin to profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  #Update a user
   def update
     @user = current_user
     if @user.update_without_password(user_params)
@@ -104,7 +103,8 @@ class UsersController < ApplicationController
     #Rails 4.0 Strong Paramaters
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation, :zip_code, :type_of_user, :user_website)
+                                   :password_confirmation, :zip_code, 
+                                   :type_of_user, :user_website)
     end
 
     # Before filters
