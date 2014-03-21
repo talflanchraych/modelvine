239410126a23ab9c993911c3251422b7ca1d0d91
Thomas Surgent <surgentt@gmail.com>
2014-03-21 11:58:35 -0400

    updates

--- app/models/access_code.rb
+++ app/models/access_code.rb
@@ -1,11 +1,15 @@
 class AccessCode < ActiveRecord::Base
 	belongs_to :user
 
-	# before_validation do
-	# 	self.code = SecureRandom.hex
-	# end
+	before_validation do
+		self.code
+	end
 
-	#validates :code, presence: true
+	validates :code, presence: true
+
+	def code
+		SecureRandom.hex
+	end
 
 	def mark_as_used(user = nil)
 		self.used_by = user.id
