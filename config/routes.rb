66f76dbd699674df110e25d3a396a2efd7d0bc8e
Thomas Surgent <surgentt@gmail.com>
2014-03-27 16:28:37 -0400

    updates routes further

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 FiscalFitness::Application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations" }
   # resources gives :users all RESTful routes
+
   resources :users do
     resources :models, :photographers, :agencies, :businesses, :makeup_artists
     member do
