083a2a8d4e633746323ffc0860382c3faf43835e
Thomas Surgent <surgentt@gmail.com>
2014-03-27 17:56:11 -0400

    Got devise routing after signup and loging working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :code_used
   end
