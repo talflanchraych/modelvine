fbcfc730027a82bc58e865a02b876a560c0289e0
Thomas Surgent <surgentt@gmail.com>
2014-03-27 17:42:47 -0400

    only let models sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -36,12 +36,13 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   # Devise Methods
-  # def after_sign_in_path_for(resource)
-  #   user_path(current_user)
-  # end
 
   def after_sign_up_path_for(resource)
     edit_user_path(current_user)
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
   end
 
 end
