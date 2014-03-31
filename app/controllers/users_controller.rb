070653e5b007d9224ae195bfeaec5bcbf3ebcd1c
Thomas Surgent <surgentt@gmail.com>
2014-03-31 13:55:54 -0400

    admin users can delete other users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
+    User.friendly.find(params[:id]).destroy
     flash[:success] = "User deleted."
     redirect_to users_url
   end
