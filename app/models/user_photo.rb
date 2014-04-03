30c241f05a812a41d9739d1a107bc8fd77f0902e
Thomas Surgent <surgentt@gmail.com>
2014-04-03 18:23:46 -0400

    userphoto fields in place, need styling

--- app/models/user_photo.rb
+++ app/models/user_photo.rb
@@ -1,7 +1,7 @@
 class UserPhoto < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :photo, :styles => { :medium => "300>x300", :thumb => "200x200#" }, :default_url => "assets/images/:style/missing.png"
+	has_attached_file :photo, :styles => { :medium => "300>x300", :thumb => "200x200#", :micro => "40x40#" }, :default_url => "assets/images/:style/missing.png"
 
 	validates :user_id, presence: true
 	validates :caption, length: { maximum: 140 }
@@ -13,7 +13,7 @@ class UserPhoto < ActiveRecord::Base
 
 	default_scope -> { order('created_at DESC')}
 
-	scope :approved_users, -> { joins(:user).where('users.approved = true') }
+	scope :approved_users, -> { joins(:user).where('users.approved = true').limit(10) }
 	
 	after_post_process :save_image_dimensions
 
@@ -22,5 +22,23 @@ class UserPhoto < ActiveRecord::Base
 	    self.photo_width = geo.width.to_i
 	    self.photo_height = geo.height.to_i
 	end
+
+	def relative_time(start_time)
+		#binding.pry
+	  diff_seconds = (Time.now - start_time).to_i
+	  case diff_seconds
+	    when 0 .. 59
+	      "#{diff_seconds} seconds ago"
+	    when 60 .. (3600-1)
+	      "#{diff_seconds/60} minutes ago"
+	    when 3600 .. (3600*24-1)
+	      "#{diff_seconds/3600} hours ago"
+	    when (3600*24) .. (3600*24*30) 
+	      "#{diff_seconds/(3600*24)} days ago"
+	    else
+	       start_time.strftime("%m/%d/%Y")
+	  end
+	end
+
 	
 end
